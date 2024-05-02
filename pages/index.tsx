7a325999950ab1eb31321b52b5a5e5c434584528
Cali Castle <hi@cali.so>
2024-05-02 20:28:01 +0800

    chore: add twitter metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,15 @@ export default defineComponent({
       },
       ogImage() {
           return '/og.png'
+      },
+      twitterTitle() {
+          return 'VueConf 2024 深圳'
+      },
+      twitterDescription() {
+          return 'VueConf 2024 将于 2024 年 7 月 6 日在中国深圳举办！作为 Vue.js 官方主办的盛会，本次大会将为全球 Vue.js 开发者带来一场前所未有的技术盛宴。'
+      },
+      twitterImage() {
+          return '/og.png'
       }
     });
   },
