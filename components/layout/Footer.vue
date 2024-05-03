234fd808c235ae1aae65bfb4185019690edc6c19
Cali Castle <hi@cali.so>
2024-05-03 23:59:27 +0800

    chore: update readme

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="my-24">
     <div class="max-w-3xl mx-auto px-4 md:px-0">
       <p class="text-center bg-clip-text text-transparent bg-gradient-to-br from-zinc-50 via-zinc-400">
-        &copy; {{ new Date().getFullYear() }} VueConf Redesign by <a href="https://cali.so>" class="text-vue-green hover:underline" target="_blank">Cali</a>. No rights reserved.
+        &copy; {{ new Date().getFullYear() }} VueConf Redesign by <a href="https://cali.so" class="text-vue-green hover:underline" target="_blank">Cali</a>. No rights reserved.
       </p>
     </div>
   </footer>
