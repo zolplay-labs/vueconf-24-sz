a99e9a25d8277c1c83c0da4e2463bd7390993a6e
Cali Castle <hi@cali.so>
2024-05-07 01:27:29 +0800

    chore: add texts

--- components/layout/Sponsors.vue
+++ components/layout/Sponsors.vue
@@ -38,10 +38,11 @@ const communities = ref<SponsorLike[]>([
 
 <template>
   <section class="my-24 px-4 md:px-0 max-w-3xl mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 group">
-    <header class="h-64 md:h-48 col-span-2 sm:col-span-3 md:col-span-4 flex justify-center items-center border border-dashed border-[--grid-color] border-b-0 relative sticky -top-0.5 z-30 bg-gradient-to-b from-[#16241B] via-[#16241B]">
+    <header class="h-64 md:h-48 col-span-2 sm:col-span-3 md:col-span-4 flex flex-col space-y-3 justify-center items-center border border-dashed border-[--grid-color] border-b-0 relative sticky -top-0.5 z-30 bg-gradient-to-b from-[#16241B] via-[#16241B]">
       <h2 class="font-bold text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-vue-green to-zinc-500 tracking-tight mix-blend-plus-lighter text-center text-balance">
         Sponsors
       </h2>
+      <p class="text-white/70 text-center">如果你想赞助本次大会，请直接与我们联系apply#fequan.com</p>
     </header>
 
     <div class="relative col-span-2 sm:col-span-3 md:col-span-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
@@ -81,10 +82,11 @@ const communities = ref<SponsorLike[]>([
       </template>
     </div>
 
-    <div class="col-span-2 sm:col-span-3 md:col-span-4 row-span-1 h-64 md:h-48 flex justify-center items-center border border-dashed border-[--grid-color] border-y-0 relative sticky -top-0.5 z-30 bg-gradient-to-b from-[#16241B] via-[#16241B]">
+    <div class="col-span-2 sm:col-span-3 md:col-span-4 row-span-1 h-64 md:h-48 flex flex-col space-y-3 justify-center items-center border border-dashed border-[--grid-color] border-y-0 relative sticky -top-0.5 z-30 bg-gradient-to-b from-[#16241B] via-[#16241B]">
       <h2 class="font-bold text-5xl md:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-vue-green to-zinc-500 tracking-tight mix-blend-plus-lighter text-center text-balance">
         Communities
       </h2>
+      <p class="text-white/70 text-center">社区支持</p>
     </div>
 
     <div class="relative col-span-2 sm:col-span-3 md:col-span-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
